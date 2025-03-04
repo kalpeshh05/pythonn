23a4b0965abd845d2bdf686d4f579940e226d92b
kalpeshh05 <sutharkk22@gmail.com>
2025-03-04 13:50:15 +0530

    uploaded

---
+++ exp.py
@@ -0,0 +1,13 @@
+/*************  ✨ Codeium Command 🌟  *************/
+a = int(input("Enter the number: "))
+b = int(input("Enter the number: "))
+a = print(input("Enter the number: "))
+b = print(input("Enter the number: "))
+
+
+sum = a + b
+
+# Print the result
+print("The sum of a and b is:", sum)
+
+/******  d4251ad2-1ee6-4e27-8305-c833004245e4  *******/
